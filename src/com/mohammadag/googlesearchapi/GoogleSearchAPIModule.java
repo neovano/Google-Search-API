6cf9b72a24b477be95977dd42aef35170295cfe1
neovano <yborgne@gmail.com>
2014-05-12 17:23:59 +0200

    Update GoogleSearchAPIModule.java

--- src/com/mohammadag/googlesearchapi/GoogleSearchAPIModule.java
+++ src/com/mohammadag/googlesearchapi/GoogleSearchAPIModule.java
@@ -12,6 +12,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.text.TextUtils;
+import android.util.Log;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
@@ -40,10 +41,16 @@ public class GoogleSearchAPIModule implements IXposedHookLoadPackage, IXposedHoo
 
 		if (!lpparam.packageName.equals(Constants.GOOGLE_SEARCH_PACKAGE))
 			return;
+		debug("Processing " + Constants.GOOGLE_SEARCH_PACKAGE);
+		hookV34(lpparam);
 
+	}
+	
+	private void hookV34(LoadPackageParam lpparam) {
 		/* IPC, not sure how many processes Google Search runs in, but we need this since
 		 * it's surely not one.
 		 */
+		debug("create BroadcastReceiver");
 		final BroadcastReceiver internalReceiver = new BroadcastReceiver() {
 			@Override
 			public void onReceive(Context context, Intent intent) {
@@ -62,16 +69,16 @@ public class GoogleSearchAPIModule implements IXposedHookLoadPackage, IXposedHoo
 			}
 		};
 
-		Class<?> Query = findClass("com.google.android.search.shared.api.Query", lpparam.classLoader);
-		Class<?> MyVoiceSearchControllerListener =
-				findClass("com.google.android.search.core.SearchController$MyVoiceSearchControllerListener", lpparam.classLoader);
-		Class<?> SearchController = findClass("com.google.android.search.core.SearchController", lpparam.classLoader);
-		Class<?> SearchResultFetcher = findClass("com.google.android.search.core.prefetch.SearchResultFetcher", lpparam.classLoader);
+		debug("findClass azs (com.google.android.search.core.SearchController)");
+		// obfuscated name of com.google.android.search.core.SearchController is azs
+		Class<?> SearchController = findClass("azs", lpparam.classLoader);
 
+		debug("hookAllConstructors azs (com.google.android.search.core.SearchController)");
 		XposedBridge.hookAllConstructors(SearchController, new XC_MethodHook() {
 			@Override
 			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
 				final Object thisObject = param.thisObject;
+				debug("getObjectField mContext on " + param.thisObject);
 				mContext = (Context) getObjectField(param.thisObject, "mContext");
 				mQueuedIntentList = new ArrayList<Intent>();
 				mContext.registerReceiver(new BroadcastReceiver() {
@@ -80,10 +87,17 @@ public class GoogleSearchAPIModule implements IXposedHookLoadPackage, IXposedHoo
 						String string = intent.getStringExtra(GoogleSearchApi.KEY_TEXT_TO_SPEAK);
 						if (TextUtils.isEmpty(string))
 							return;
-
+						debug("received string " + string);
+						
+						// mVoiceSearchServices has the same name in "azs" but has type "fio"
+						debug("getObjectField mVoiceSearchServices on " + thisObject);
 						Object mVoiceSearchServices = getObjectField(thisObject, "mVoiceSearchServices");
-						Object ttsManager = XposedHelpers.callMethod(mVoiceSearchServices, "getLocalTtsManager"); 
-						Method method = XposedHelpers.findMethodBestMatch(ttsManager.getClass(), "enqueue", String.class, Runnable.class);
+						// getLocalTtsManager obfuscated name is "asi"
+						debug("callMethod asi() on " + mVoiceSearchServices);
+						Object ttsManager = XposedHelpers.callMethod(mVoiceSearchServices, "asi");
+						// LocalTtsManager class is now called "geg", "enqueue" is now "a"
+						debug("findMethodBestMatch a(String, Runnable) on " + ttsManager.getClass());
+						Method method = XposedHelpers.findMethodBestMatch(ttsManager.getClass(), "a", String.class, Runnable.class);
 						try {
 							method.invoke(ttsManager, string, null);
 						} catch (Exception e) {
@@ -101,15 +115,31 @@ public class GoogleSearchAPIModule implements IXposedHookLoadPackage, IXposedHoo
 			}
 		});
 
-		findAndHookMethod(SearchResultFetcher, "obtainSearchResult", Query, new XC_MethodHook() {
+
+		debug("findClass com.google.android.shared.search.Query (com.google.android.search.shared.api.Query)");
+		// com.google.android.search.shared.api.Query is now com.google.android.shared.search.Query
+		Class<?> Query = findClass("com.google.android.shared.search.Query", lpparam.classLoader);
+
+		debug("findClass blq (com.google.android.search.core.prefetch.SearchResultFetcher)");
+		// com.google.android.search.core.prefetch.SearchResultFetcher is now "blq"
+		Class<?> SearchResultFetcher = findClass("blq", lpparam.classLoader);
+		// obtainSearchResult is now "s"
+		debug("findAndHookMethod s(Query) on " + SearchResultFetcher);
+		findAndHookMethod(SearchResultFetcher, "s", Query, new XC_MethodHook() {
 			@Override
 			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 				Object queryResult = param.args[0];
+				// mQueryChars is now "bjt"
+				debug("getObjectField bjt on " + queryResult);
 				CharSequence searchQueryText =
-						(CharSequence) getObjectField(queryResult, "mQueryChars");
+						(CharSequence) getObjectField(queryResult, "bjt");
+				debug("getObjectField mCache on " + param.thisObject);
 				Object mCache = getObjectField(param.thisObject, "mCache");
+				debug("getObjectField mClock on " + param.thisObject);
 				Object mClock = getObjectField(param.thisObject, "mClock");
-				Object mCachedResult = XposedHelpers.callMethod(mCache, "get", queryResult,
+				// get is now "a"
+				debug("callMethod a(?, elapsedRealtime, true) on " + mCache);
+				Object mCachedResult = XposedHelpers.callMethod(mCache, "a", queryResult,
 						XposedHelpers.callMethod(mClock, "elapsedRealtime"),
 						true);
 				
@@ -129,40 +159,62 @@ public class GoogleSearchAPIModule implements IXposedHookLoadPackage, IXposedHoo
 					broadcastGoogleSearch(mContext, searchQueryText, false,
 							mPreferences.getBoolean(Constants.KEY_DELAY_BROADCASTS, false));
 				} else {
-					XposedBridge.log(String.format("Google Search API: New Search detected: %s",
+					debug(String.format("Google Search API: New Search detected: %s",
 							searchQueryText.toString()));
 				}
 			}
 		});
 
-		XposedBridge.hookAllMethods(MyVoiceSearchControllerListener, "onRecognitionResult", new XC_MethodHook() {
+		debug("findClass bae (com.google.android.search.core.SearchController$MyVoiceSearchControllerListener)");
+		// com.google.android.search.core.SearchController$MyVoiceSearchControllerListener is now "bae"
+		Class<?> MyVoiceSearchControllerListener =
+				findClass("bae", lpparam.classLoader);
+		// onRecognitionResult is now "public final void a(CharSequence paramCharSequence, glq paramglq, blk paramblk)"
+		
+		debug("findClass glq");
+		Class<?> glq = findClass("glq", lpparam.classLoader);
+		debug("findClass blk");
+		Class<?> blk = findClass("blk", lpparam.classLoader);
+		
+		debug("findMethodBestMatch a(CharSequence, " + glq + ", " + blk + ") on " + MyVoiceSearchControllerListener);
+		Method method = XposedHelpers.findMethodBestMatch(MyVoiceSearchControllerListener, "a", CharSequence.class, glq, blk);
+		debug("hookMethod " + method);
+		XposedBridge.hookMethod(method, new XC_MethodHook() {
 			@Override
 			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 				CharSequence voiceResult = (CharSequence) param.args[0];
 				mPreferences.reload();
 				if (mContext != null) {
+					debug("broadcastGoogleSearch " + voiceResult.toString());
 					broadcastGoogleSearch(mContext, voiceResult, true,
 							mPreferences.getBoolean(Constants.KEY_DELAY_BROADCASTS, false));
 				} else {
-					XposedBridge.log(voiceResult.toString());
+					debug(voiceResult.toString());
 				}
 			}
 		});
-
+		
 		/* GEL workaround, GEL opens Google Search eventually, so this will overlay whatever
 		 * activity a developer has made. This broadcasts intents after the window has gained focus.
 		 */
-		findAndHookMethod("com.google.android.search.gel.SearchOverlayImpl", lpparam.classLoader,
+		debug("findAndHookMethod ccu onWindowFocusChanged(boolean) (com.google.android.search.gel.SearchOverlayImpl)");
+		// com.google.android.search.gel.SearchOverlayImpl is now "ccu"
+		findAndHookMethod("ccu", lpparam.classLoader,
 				"onWindowFocusChanged", boolean.class, new XC_MethodHook() {
 			@Override
 			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
 				boolean hasFocus = (Boolean) param.args[0];
-
+				debug("getObjectField mContext on " + param.thisObject);
 				Context context = (Context) getObjectField(param.thisObject, "mContext");
 				if (context != null && !hasFocus)
 					context.sendBroadcast(new Intent(Constants.INTENT_FLUSH_INTENTS));
 			}
-		});
+		});		
+	}
+
+	private void debug(String msg) {
+		Log.d("GoogleSearchAPI", msg);
+		XposedBridge.log(msg);
 	}
 
 	private void touchOurself(ClassLoader classLoader) {
